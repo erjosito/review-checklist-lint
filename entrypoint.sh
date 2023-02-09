fb3592a4e15d00b43036865635343e10998cd588
Jose Moreno <jose.moreno@microsoft.com>
2023-02-09 22:59:56 +0100

    some real logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,15 @@
 language="$1"
 file_list=$(ls -R1 **/*.$language.json)
 number_of_checklists=$(echo $file_list | wc -l)
+failed_tests=0
+while read -r file; do
+    all_guids=$(cat $file | jq -r '.items[].guid' | wc -l)
+    unique_guids=$(cat $file | jq -r '.items[].guid' | sort -u | wc -l)
+    if [[ "$all_guids" == "$unique_guids" ]]; then
+        echo "File $file has $all_guids, and all are unique"
+    else
+        echo "File $file has $all_guids, but only $unique_guids are unique"
+        failed_tests=$((failed_tests+1))
+    fi
+done <<< "$file_list"
 echo "number_of_checklists=$number_of_checklists" >> $GITHUB_OUTPUT
