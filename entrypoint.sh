723b245a7b4dff3d3226dfb0d59e831cb98bc7b7
Jose Moreno <jose.moreno@microsoft.com>
2023-02-09 23:13:48 +0100

    bug fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,13 @@ file_list=$(ls -R1 **/*.$language.json)
 number_of_checklists=$(echo $file_list | wc -l)
 failed_tests=0
 while read -r file; do
-    all_guids=$(cat $file | jq -r '.items[].guid' | wc -l)
-    unique_guids=$(cat $file | jq -r '.items[].guid' | sort -u | wc -l)
+    echo "Processing $file..."
+    all_guids=$(cat $file | jq -r 'try .items[].guid' | wc -l)
+    unique_guids=$(cat $file | jq -r 'try .items[].guid' | sort -u | wc -l)
     if [[ "$all_guids" == "$unique_guids" ]]; then
-        echo "File $file has $all_guids, and all are unique"
+        echo "File $file has $all_guids GUIDs, and all are unique"
     else
-        echo "File $file has $all_guids, but only $unique_guids are unique"
+        echo "File $file has $all_guids GUIDs, but only $unique_guids are unique"
         failed_tests=$((failed_tests+1))
     fi
 done <<< "$file_list"
