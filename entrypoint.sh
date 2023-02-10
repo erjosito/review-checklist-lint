baa4ab5aec67cea10943fe304fc72b5e0424459d
Jose Moreno <jose.moreno@microsoft.com>
2023-02-10 16:33:30 +0100

    added category check

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,42 +9,51 @@ file_list=$(eval "ls -R1 */*.$file_extension")
 number_of_checklists=$(echo $file_list | wc -l)
 
 # Info message
-echo "Running with file_extension=$file_extension, key_name=$key_name, criteria_key=$criteria_key, criteria_value=$criteria_value"
+echo "DEBUG: Running with file_extension=$file_extension, key_name=$key_name, criteria_key=$criteria_key, criteria_value=$criteria_value"
 
 # Check JSON field uniqueness (GUID for azure checklists)
-failed_tests=0
 performed_tests=0
+unique_failed_tests=0
+unique_failed_tests_not_counted=0
 IFS='
 '
 for file in $file_list; do
+    echo "DEBUG: Processing $file..."
+    performed_tests=$((performed_tests+1))
     # Figure out whether we want to process this specific one, if criteria were specified
     process=yes
     if [ -n "$criteria_key" -a -n "$criteria_value" ]; then
         file_criteria_value=$(cat $file | jq -r "$criteria_key")
         if [ "$file_criteria_value" != "$criteria_value" ]; then
-            echo "File $file: retrieved criteria value $file_criteria_value does not match criteria $criteria_value (JSON key $criteria_key)"
+            echo "DEBUG: File $file: retrieved criteria value $file_criteria_value does not match criteria $criteria_value (JSON key $criteria_key)"
             process=no
         fi
     fi
-    if [ "$process" == "yes" ]; then
-        performed_tests=$((performed_tests+1))
-        # Process file
-        echo "Processing $file..."
-        all_guids=$(cat $file | jq -r "try .. | objects | select( .$key_name) | .$key_name" | wc -l)
-        unique_guids=$(cat $file | jq -r "try .. | objects | select( .$key_name) | .$key_name" | sort -u | wc -l)
-        # Compare values
-        if [ "$all_guids" -eq "$unique_guids" ]; then
-            echo "File $file has $all_guids GUIDs, and all are unique"
-        else
-            echo "File $file has $all_guids GUIDs, but only $unique_guids are unique"
-            failed_tests=$((failed_tests+1))
-        fi
+    # Process file for unique keys
+    all_guids=$(cat $file | jq -r "try .. | objects | select( .$key_name) | .$key_name" | wc -l)
+    unique_guids=$(cat $file | jq -r "try .. | objects | select( .$key_name) | .$key_name" | sort -u | wc -l)
+    if [ "$all_guids" -eq "$unique_guids" ]; then
+        echo "INFO: File $file has $all_guids GUIDs, and all are unique"
     else
-        echo "Skipping file $file, not matching criteria ($criteria_key = $criteria_value)"
+        echo "INFO: File $file has $all_guids GUIDs, but only $unique_guids are unique"
+        if [ "$process" == "yes" ]; then
+            unique_failed_tests=$((failed_tests+1))
+        else
+            unique_failed_tests_not_counted=$((failed_tests_not_counted+1))
+        fi
+    fi
+    # Check categories
+    categories=$(cat $file | jq -r "try .. | objects | select( .$key_name) | .$key_name" | sort -u)
+    category_list=$(cat $file | jq -r ".categories[].name" | sort -u)
+    if [ "$categories" == "$category_list" ]; then
+        echo "INFO: categories in checklist $file match with the categories defined in the checks"
+    else
+        echo "INFO: there is a discrepancy in the categories defined in file $file."
     fi
 done
-echo "$performed_tests files verified, $failed_tests FAILED lint checks"
+echo "DEBUG: $performed_tests files verified, $unique_failed_tests FAILED lint checks ($unique_failed_tests_not_counted files failed the check, but did not match the criteria)"
 echo "number_of_checklists=$number_of_checklists" >> $GITHUB_OUTPUT
-if [ "$failed_tests" -gt 0 ]; then
+# Break the test only if there are not unique GUIDs for a checklist matching the criteria
+if [ "$unique_failed_tests" -gt 0 ]; then
     exit 1
 fi
